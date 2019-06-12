4a10063881234ce66f06376ef277d46b66615ca8
devcnn3c <dchauhan@3clogic.com>
2019-06-12 18:42:27 +0530

    Initial commit 6

--- app.js
+++ app.js
@@ -3,20 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const simpleGit = require('simple-git/promise')();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const USER = 'devcnn3c';
-const PASS = 'yrudumb123';
-const REPO = 'git@github.com:devcnn3c/simple.git';
 
+const gitP = require('simple-git/promise');
+const git = gitP(__dirname);
+const remote = `https://github.com/devcnn3c/simple.git`;
 
-const git = require('simple-git/promise');
-
-
-const remote = `${REPO}`;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -29,33 +24,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/git/*', (req,res,next) => {
+app.get('/git/init', (req,res,next) => {
 console.log('Git auth');
-  git().silent(false).
-      clone(remote, 'C:\\simple')
-      .then(() => console.log('finished'))
-      next()
-      .catch((err) => console.error('failed: ', err));
+  git.checkIsRepo()
+      .then(isRepo => console.log('ISREPO:',isRepo) && !isRepo && initialiseRepo(git))
+      .then(() => git.fetch());
+  initialiseRepo(git);
+  function initialiseRepo (git) {
+    console.log('initialize');
+    return git.init()
+        .then(() => git.addRemote('origin', 'https://github.com/devcnn3c/simple.git'))
+  }
  })
-app.get('/git/init', (req,res) => {
-  try {
-    git()
-        .add('./*')
-        .commit("first commit!")
-        .addRemote('origin', remote)
-        .push('origin', 'master');
 
-    /*simpleGit.init()
-        .add('./*')
-        .commit("first commit!")
-        .addRemote('origin', remote)
-        .push('origin', 'master');
-  */
-  }
+app.get('/git/add', (req,res) => {
+git.add('./*').then(result=>{console.log('add:', result)
+res.status(200).send('Added')});
+});
 
-  catch(err){
-    console.log('ERRor:',err);
-  }
+app.get('/git/pull', (req,res) => {
+
 
 })
 // catch 404 and forward to error handler
